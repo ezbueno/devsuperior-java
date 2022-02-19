ceef5978fb182c32358ffe8b3d6625434f0ac711
ezbueno <ezandrobueno@yahoo.com.br>
2022-02-18 21:04:17 -0300

    Casting

---
+++ estrutura-sequencial/src/casting/Main.java
@@ -0,0 +1,11 @@
+package casting;
+
+public class Main {
+	public static void main(String[] args) {
+		int a = 5;
+		int b = 2;
+		double resultado = (double) a / b;
+		
+		System.out.println(resultado);
+	}
+}
