930c2ce150dde8f23903193c5cd0fcf00d393eec
ezbueno <ezandrobueno@yahoo.com.br>
2022-02-21 19:11:41 -0300

    Escopo e inicialização

---
+++ estrutura-condicional/src/escopo/inicializacao/Main.java
@@ -0,0 +1,16 @@
+package escopo.inicializacao;
+
+public class Main {
+	public static void main(String[] args) {
+		double price = 400.00;
+		double discount;
+
+		if (price < 200.00) {
+			discount = price * 0.1;
+		} else {
+			discount = 0.0;
+		}
+
+		System.out.println(discount);
+	}
+}
