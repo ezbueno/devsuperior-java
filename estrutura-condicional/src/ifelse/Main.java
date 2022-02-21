273b47c3af9542d3efd3226f296a8d6aeb313546
ezbueno <ezandrobueno@yahoo.com.br>
2022-02-21 17:57:59 -0300

    Estrutura condicional

---
+++ estrutura-condicional/src/ifelse/Main.java
@@ -0,0 +1,20 @@
+package ifelse;
+
+import java.util.Scanner;
+
+public class Main {
+	public static void main(String[] args) {
+		try (Scanner sc = new Scanner(System.in)) {
+			System.out.println("Quantas horas?");
+			int hora = sc.nextInt();
+
+			if (hora < 12) {
+				System.out.println("Bom dia!");
+			} else if (hora < 18) {
+				System.out.println("Boa tarde!");
+			} else {
+				System.out.println("Boa noite!");
+			}
+		}
+	}
+}
