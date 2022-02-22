42544473a35da799906d2e9486d5194312072ddd
ezbueno <ezandrobueno@yahoo.com.br>
2022-02-22 18:10:30 -0300

    Exercício

--- estrutura-condicional/src/exercicios/propostos/ExercicioCinco.java
+++ estrutura-condicional/src/exercicios/propostos/ExercicioCinco.java
@@ -6,8 +6,8 @@ package exercicios.propostos;
  * seguir, calcule e mostre o valor da conta a pagar.
  * Código 	Especificação		Preço
  * 1		Cachorro Quente		R$ 4.00
- * 2		X-Salada			R$ 4.50
- * 3		X-Bacon				R$ 5.00
+ * 2		X-Salada		R$ 4.50
+ * 3		X-Bacon			R$ 5.00
  * 4		Torrada Simples		R$ 2.00
  * 5		Refrigerante		R$ 1.50
  * Entrada:
