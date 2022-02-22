4bc7b4e77ae7101544b0ccc4f00515b2691a1cd0
ezbueno <ezandrobueno@yahoo.com.br>
2022-02-22 18:08:59 -0300

    Exercício

--- estrutura-condicional/src/exercicios/propostos/ExercicioCinco.java
+++ estrutura-condicional/src/exercicios/propostos/ExercicioCinco.java
@@ -4,12 +4,12 @@ package exercicios.propostos;
  * @author Ezandro Bueno
  * Com base na tabela abaixo, escreva um programa que leia o código de um item e a quantidade deste item. A 
  * seguir, calcule e mostre o valor da conta a pagar.
- * Código 	Especificação		Preço
- * 1		Cachorro Quente		R$ 4.00
- * 2		X-Salada			R$ 4.50
- * 3		X-Bacon				R$ 5.00
- * 4		Torrada Simples		R$ 2.00
- * 5		Refrigerante		R$ 1.50
+ * Código 	Especificação			Preço
+ * 1		Cachorro Quente			R$ 4.00
+ * 2		X-Salada				R$ 4.50
+ * 3		X-Bacon					R$ 5.00
+ * 4		Torrada Simples			R$ 2.00
+ * 5		Refrigerante			R$ 1.50
  * Entrada:
  * 3
  * 2
