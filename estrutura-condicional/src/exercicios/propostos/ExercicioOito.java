4992f075117c00d9316362ce75868be1b31d7d3f
ezbueno <ezandrobueno@yahoo.com.br>
2022-02-23 22:20:12 -0300

    Exercício resolvido

--- estrutura-condicional/src/exercicios/propostos/ExercicioOito.java
+++ estrutura-condicional/src/exercicios/propostos/ExercicioOito.java
@@ -1,5 +1,8 @@
 package exercicios.propostos;
 
+import java.util.Locale;
+import java.util.Scanner;
+
 /**
  * @author Ezandro Bueno
  * Em um país imaginário denominado Lisarb, todos os habitantes ficam felizes em pagar seus impostos, pois sabem 
@@ -15,7 +18,7 @@ package exercicios.propostos;
  * Lembre que, se o salário for R$ 3002.00, a taxa que incide é de 8% apenas sobre R$ 1000.00, pois a faixa de 
  * salário que fica de R$ 0.00 até R$ 2000.00 é isenta de Imposto de Renda. No exemplo fornecido (abaixo), a taxa é 
  * de 8% sobre R$ 1000.00 + 18% sobre R$ 2.00, o que resulta em R$ 80.36 no total. O valor deve ser impresso com 
- * duas casas decimais.
+ * duas casas decimais, conforme exemplos.
  * Entrada:
  * 3002.00
  * Saída:
@@ -31,6 +34,28 @@ package exercicios.propostos;
  */
 public class ExercicioOito {
 	public static void main(String[] args) {
-		
+		Locale.setDefault(Locale.US);
+
+		try (Scanner sc = new Scanner(System.in)) {
+			double salario = sc.nextDouble();
+
+			double valorImposto = 0.0;
+
+			if (salario <= 2000.0) {
+				System.out.println("Isento");
+			} else if (salario <= 3000.0) {
+				salario -= 2000.0;
+				valorImposto = (0.08 * salario);
+				System.out.printf("R$ %.2f%n", valorImposto);
+			} else if (salario <= 4500.0) {
+				salario -= 3000.0;
+				valorImposto = (0.08 * 1000.0) + (0.18 * salario);
+				System.out.printf("R$ %.2f%n", valorImposto);
+			} else {
+				salario -= 4500.0;
+				valorImposto = (0.08 * 1000.0) + (0.18 * 1500.0) + (0.28 * salario);
+				System.out.printf("R$ %.2f%n", valorImposto);
+			}
+		}
 	}
 }
