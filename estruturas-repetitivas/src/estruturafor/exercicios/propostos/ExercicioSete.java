f18f65e41aa1ff803f3052be7f4ad803b8e4f92b
ezbueno <ezandrobueno@yahoo.com.br>
2022-02-27 18:22:22 -0300

    Exercício resolvido

--- estruturas-repetitivas/src/estruturafor/exercicios/propostos/ExercicioSete.java
+++ estruturas-repetitivas/src/estruturafor/exercicios/propostos/ExercicioSete.java
@@ -1,5 +1,7 @@
 package estruturafor.exercicios.propostos;
 
+import java.util.Scanner;
+
 /**
  * @author Ezandro Bueno
  * Fazer um programa para ler um número inteiro positivo N. O programa deve então mostrar na tela N linhas, 
@@ -15,5 +17,13 @@ package estruturafor.exercicios.propostos;
  * 5 25 125
  */
 public class ExercicioSete {
+	public static void main(String[] args) {
+		try (Scanner sc = new Scanner(System.in)) {
+			int n = sc.nextInt();
 
+			for (int i = 1; i <= n; i++) {
+				System.out.print(i + " " + (int) Math.pow(i, 2.0) + " " + (int) Math.pow(i, 3.0) + "\n");
+			}
+		}
+	}
 }
