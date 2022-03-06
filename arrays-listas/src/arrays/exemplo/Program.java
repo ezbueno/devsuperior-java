7842067c177d791b91163991be161a47fc1af28d
ezbueno <ezandrobueno@yahoo.com.br>
2022-03-06 17:58:47 -0300

    Vetores - Parte 1

---
+++ arrays-listas/src/arrays/exemplo/Program.java
@@ -0,0 +1,38 @@
+package arrays.exemplo;
+
+import java.util.Locale;
+import java.util.Scanner;
+
+/**
+ * @author Ezandro Bueno
+ * Fazer um programa para ler um número inteiro N e a altura de N pessoas. Armazene as N alturas
+ * em um vetor. Em seguida, mostrar a altura média dessas pessoas.
+ * Input:
+ * 3
+ * 1.72
+ * 1.56
+ * 1.80
+ * Output:
+ * AVERAGE HEIGHT = 1.69
+ */
+public class Program {
+	public static void main(String[] args) {
+		Locale.setDefault(Locale.US);
+
+		try (Scanner sc = new Scanner(System.in)) {
+			int n = sc.nextInt();
+
+			double[] vect = new double[n];
+			double sum = 0.0;
+			double average;
+
+			for (int i = 0; i < vect.length; i++) {
+				vect[i] = sc.nextDouble();
+				sum += vect[i];
+			}
+
+			average = sum / vect.length;
+			System.out.printf("AVERAGE HEIGHT = %.2f%n", average);
+		}
+	}
+}
