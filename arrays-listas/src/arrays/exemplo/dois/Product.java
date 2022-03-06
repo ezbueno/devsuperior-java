6871c7758b60228f3f4fc41148ba46c115c98603
ezbueno <ezandrobueno@yahoo.com.br>
2022-03-06 20:00:54 -0300

    Vetores - Parte 2

--- arrays-listas/src/arrays/exemplo/dois/Product.java
+++ arrays-listas/src/arrays/exemplo/dois/Product.java
@@ -3,15 +3,12 @@ package arrays.exemplo.dois;
 public class Product {
 	private String name;
 	private double price;
-	
-	public Product() {
-	}
 
 	public Product(String name, double price) {
 		this.name = name;
 		this.price = price;
 	}
-	
+
 	public String getName() {
 		return this.name;
 	}
