531fc9b80bf01e49fcaacc676635bd82e4a97982
ezbueno <ezandrobueno@yahoo.com.br>
2022-03-09 20:59:44 -0300

    Estrutura de repetição foreach

---
+++ arrays-listas/src/foreach/Program.java
@@ -0,0 +1,11 @@
+package foreach;
+
+public class Program {
+	public static void main(String[] args) {
+		String[] vect = new String[] { "Maria", "Bob", "Alex" };
+
+		for (String v : vect) {
+			System.out.println(v);
+		}
+	}
+}
