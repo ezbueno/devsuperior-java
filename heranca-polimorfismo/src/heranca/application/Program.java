6b3afb150a4627fad4a4892736ccd1ddd9f935a5
ezbueno <ezandrobueno@yahoo.com.br>
2022-04-01 21:08:49 -0300

    Upcasting e Downcasting

---
+++ heranca-polimorfismo/src/heranca/application/Program.java
@@ -0,0 +1,33 @@
+package heranca.application;
+
+import heranca.entities.Account;
+import heranca.entities.BusinessAccount;
+import heranca.entities.SavingsAccount;
+
+public class Program {
+	public static void main(String[] args) {
+		Account acc = new Account(1001, "Alex", 0.0);
+		BusinessAccount bacc = new BusinessAccount(1002, "Maria", 0.0, 500.0);
+		
+		/* UPCASTING */
+		Account acc2 = bacc;
+		Account acc3 = new BusinessAccount(1003, "Bob", 0.0, 200.0);
+		Account acc4 = new SavingsAccount(1004, "Anna", 0.0, 0.01);
+		
+		/* DOWNCASTING */
+		BusinessAccount bacc2 = (BusinessAccount) acc3;
+		bacc2.loan(100.0);
+			
+		if (acc4 instanceof BusinessAccount ba) {
+			ba = (BusinessAccount) acc4;
+			ba.loan(200.0);
+			System.out.println("Loan!");
+		}
+		
+		if (acc4 instanceof SavingsAccount sa) {
+			sa = (SavingsAccount) acc4;
+			sa.updateBalance();
+			System.out.println("Update!");
+		}
+	}
+}
