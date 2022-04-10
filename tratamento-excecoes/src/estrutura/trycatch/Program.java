072675e14968e083596ceebf726448e839473275
ezbueno <ezandrobueno@yahoo.com.br>
2022-04-10 17:37:12 -0300

    Pilha de chamadas de métodos (stack trace)

--- tratamento-excecoes/src/estrutura/trycatch/Program.java
+++ tratamento-excecoes/src/estrutura/trycatch/Program.java
@@ -5,15 +5,30 @@ import java.util.Scanner;
 
 public class Program {
 	public static void main(String[] args) {
+		method1();
+		System.out.println("End of program!");
+	}
+
+	private static void method1() {
+		System.out.println("*** METHOD1 START ***");
+		method2();
+		System.out.println("*** METHOD1 END ***");
+	}
+
+	private static void method2() {
+		System.out.println("*** METHOD2 START ***");
+
 		try (Scanner sc = new Scanner(System.in)) {
 			String[] vect = sc.nextLine().split(" ");
 			int position = sc.nextInt();
 			System.out.println(vect[position]);
 		} catch (ArrayIndexOutOfBoundsException e) {
 			System.out.println("Invalid position!");
+			e.printStackTrace();
 		} catch (InputMismatchException e) {
 			System.out.println("Input error!");
 		}
-		System.out.println("End of program!");
+
+		System.out.println("*** METHOD2 END ***");
 	}
 }
