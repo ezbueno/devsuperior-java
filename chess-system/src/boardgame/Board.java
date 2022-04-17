2bb50c41d80d1355d8d04d3a06306661ddd2e654
ezbueno <ezandrobueno@yahoo.com.br>
2022-04-17 15:30:35 -0300

    BoardException e programação defensiva

--- chess-system/src/boardgame/Board.java
+++ chess-system/src/boardgame/Board.java
@@ -1,11 +1,17 @@
 package boardgame;
 
 public class Board {
+	private static final String POSITION_NOT_ON_THE_BOARD = "Position not on the board";
+
 	private int rows;
 	private int columns;
 	private Piece[][] pieces;
 
 	public Board(int rows, int columns) {
+		if (rows < 1 || columns < 1) {
+			throw new BoardException("Error creating board: there must be at least 1 row and 1 column");
+		}
+
 		this.rows = rows;
 		this.columns = columns;
 		this.pieces = new Piece[rows][columns];
@@ -15,28 +21,45 @@ public class Board {
 		return this.rows;
 	}
 
-	public void setRows(int rows) {
-		this.rows = rows;
-	}
-
 	public int getColumns() {
 		return this.columns;
 	}
 
-	public void setColumns(int columns) {
-		this.columns = columns;
-	}
-
 	public Piece piece(int row, int column) {
+		if (!this.positionExists(row, column)) {
+			throw new BoardException(POSITION_NOT_ON_THE_BOARD);
+		}
 		return this.pieces[row][column];
 	}
 
 	public Piece piece(Position position) {
+		if (!this.positionExists(position)) {
+			throw new BoardException(POSITION_NOT_ON_THE_BOARD);
+		}
 		return this.pieces[position.getRow()][position.getColumn()];
 	}
-	
+
 	public void placePiece(Piece piece, Position position) {
+		if (this.thereIsAPiece(position)) {
+			throw new BoardException("There is already a piece on position " + position);
+		}
+
 		this.pieces[position.getRow()][position.getColumn()] = piece;
 		piece.position = position;
+	}
+
+	public boolean positionExists(Position position) {
+		return this.positionExists(position.getRow(), position.getColumn());
+	}
+
+	private boolean positionExists(int row, int column) {
+		return row >= 0 && row < this.rows && column >= 0 && column < this.columns;
+	}
+
+	public boolean thereIsAPiece(Position position) {
+		if (!this.positionExists(position)) {
+			throw new BoardException(POSITION_NOT_ON_THE_BOARD);
+		}
+		return this.piece(position) != null;
 	}
 }
