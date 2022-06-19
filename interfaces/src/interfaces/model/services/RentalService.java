96e9e4de42f6b79a9bb8659202dde3016237a8ef
ezbueno <ezandrobueno@yahoo.com.br>
2022-06-19 17:47:12 -0300

    Interface - Exercício

---
+++ interfaces/src/interfaces/model/services/RentalService.java
@@ -0,0 +1,34 @@
+package interfaces.model.services;
+
+import interfaces.model.entities.CarRental;
+import interfaces.model.entities.Invoice;
+
+public class RentalService {
+	private Double pricePerDay;
+	private Double pricePerHour;
+	private TaxService taxService;
+
+	public RentalService(Double pricePerDay, Double pricePerHour, TaxService taxService) {
+		this.pricePerDay = pricePerDay;
+		this.pricePerHour = pricePerHour;
+		this.taxService = taxService;
+	}
+
+	public void processInvoice(CarRental carRental) {
+		long t1 = carRental.getStart().getTime();
+		long t2 = carRental.getFinish().getTime();
+		double hours = (double) (t2 - t1) / 1000 / 60 / 60;
+
+		double basicPayment;
+
+		if (hours <= 12.0) {
+			basicPayment = this.pricePerHour * Math.ceil(hours);
+		} else {
+			basicPayment = this.pricePerDay * Math.ceil(hours / 24);
+		}
+
+		double tax = this.taxService.tax(basicPayment);
+
+		carRental.setInvoice(new Invoice(basicPayment, tax));
+	}
+}
