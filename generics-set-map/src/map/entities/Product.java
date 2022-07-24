8a7dabe61c637f3aaf5abf424ae6a17621c4d14a
ezbueno <ezandrobueno@yahoo.com.br>
2022-07-24 14:18:22 -0300

    Map

---
+++ generics-set-map/src/map/entities/Product.java
@@ -0,0 +1,46 @@
+package map.entities;
+
+import java.util.Objects;
+
+public class Product {
+	private String name;
+	private Double price;
+
+	public Product(String name, Double price) {
+		this.name = name;
+		this.price = price;
+	}
+
+	public String getName() {
+		return this.name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public Double getPrice() {
+		return this.price;
+	}
+
+	public void setPrice(Double price) {
+		this.price = price;
+	}
+
+	@Override
+	public int hashCode() {
+		return Objects.hash(this.name, this.price);
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (this.getClass() != obj.getClass())
+			return false;
+		Product other = (Product) obj;
+		return Objects.equals(this.name, other.name) && Objects.equals(this.price, other.price);
+	}
+}
