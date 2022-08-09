fc148b6f6398fe21104283ec3666d502b7b4d697
ezbueno <ezandrobueno@yahoo.com.br>
2022-08-09 14:31:44 -0300

    Consumer

--- programacao-funcional-expressoes-lambda/src/consumer/application/Program.java
+++ programacao-funcional-expressoes-lambda/src/consumer/application/Program.java
@@ -14,16 +14,16 @@ import consumer.entities.Product;
 public class Program {
 	public static void main(String[] args) {
 		Locale.setDefault(Locale.US);
-		
+
 		List<Product> list = new ArrayList<>();
-		
+
 		list.add(new Product("TV", 900.00));
 		list.add(new Product("Mouse", 50.00));
 		list.add(new Product("Tablet", 350.50));
 		list.add(new Product("HD Case", 80.90));
-		
+
 		double factor = 1.1;
-		
+
 		list.forEach(p -> p.setPrice(p.getPrice() * factor));
 		list.forEach(System.out::println);
 	}
