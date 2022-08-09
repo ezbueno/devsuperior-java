9f9e922d2510df23678610bb2ab9180845bf2a06
ezbueno <ezandrobueno@yahoo.com.br>
2022-08-09 13:34:32 -0300

    Comparator

--- programacao-funcional-expressoes-lambda/src/comparator/application/MyComparator.java
+++ programacao-funcional-expressoes-lambda/src/comparator/application/MyComparator.java
@@ -5,7 +5,7 @@ import java.util.Comparator;
 import comparator.entities.Product;
 
 public class MyComparator implements Comparator<Product> {
-	@Override
+	@Override 
 	public int compare(Product p1, Product p2) {
 		return p1.getName().toUpperCase().compareTo(p2.getName().toUpperCase());
 	}
