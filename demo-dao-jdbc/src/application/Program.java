1460db5f95d7f28373974965ba3966afdaacfd7b
ezbueno <ezandrobueno@yahoo.com.br>
2022-10-05 14:04:26 -0300

    Implementação do método insert

--- demo-dao-jdbc/src/application/Program.java
+++ demo-dao-jdbc/src/application/Program.java
@@ -1,5 +1,6 @@
 package application;
 
+import java.util.Date;
 import java.util.List;
 
 import model.dao.DaoFactory;
@@ -10,22 +11,23 @@ import model.entities.Seller;
 public class Program {
 	public static void main(String[] args) {
 		SellerDao sellerDao = DaoFactory.createSellerDao();
-		
-		System.out.println("=== TEST 1: Seller findById ===");		
+
+		System.out.println("=== TEST 1: Seller findById ===");
 		Seller seller = sellerDao.findById(3);
 		System.out.println(seller);
-		
-		System.out.println();
-		
-		System.out.println("=== TEST 2: Seller findByDepartment ===");
+
+		System.out.println("\n=== TEST 2: Seller findByDepartment ===");
 		Department department = new Department(2, null);
 		List<Seller> list = sellerDao.findByDepartment(department);
 		list.forEach(System.out::println);
-		
-		System.out.println();
-		
-		System.out.println("=== TEST 3: Seller findAll ===");
+
+		System.out.println("\n=== TEST 3: Seller findAll ===");
 		list = sellerDao.findAll();
 		list.forEach(System.out::println);
+
+		System.out.println("\n=== TEST 4: Seller insert ===");
+		Seller newSeller = new Seller(null, "Greg", "greg@gmail.com", new Date(), 4000.0, department);
+		sellerDao.insert(newSeller);
+		System.out.println("Inserted! New id = " + newSeller.getId());
 	}
 }
