7ca6ace6656ac9347b61c81997b94819cb20faf9
ezbueno <ezandrobueno@yahoo.com.br>
2022-10-20 14:55:21 -0300

    JPA-Hibernate - Parte 3

--- aula-jpa-maven/src/main/java/aplicacao/Programa.java
+++ aula-jpa-maven/src/main/java/aplicacao/Programa.java
@@ -1,15 +1,24 @@
 package aplicacao;
 
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.Persistence;
+
 import dominio.Pessoa;
 
 public class Programa {
 	public static void main(String[] args) {
-		Pessoa p1 = new Pessoa(1, "Carlos da Silva", "carlos@gmail.com");
-		Pessoa p2 = new Pessoa(2, "Joaquim Torres", "joaquims@gmail.com");
-		Pessoa p3 = new Pessoa(13, "Ana Maria", "ana@gmail.com");
+		EntityManagerFactory emf = Persistence.createEntityManagerFactory("aula-jpa-maven");
+		EntityManager em = emf.createEntityManager();
 
-		System.out.println(p1);
-		System.out.println(p2);
-		System.out.println(p3);
+		Pessoa p = em.find(Pessoa.class, 2);
+		
+		em.getTransaction().begin();
+		em.remove(p);
+		em.getTransaction().commit();
+		
+		System.out.println("Pronto!");
+		em.close();
+		emf.close();
 	}
 }
