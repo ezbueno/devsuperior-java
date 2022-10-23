f798f5f9ab0212af4540392c8dbc0909131880f2
ezbueno <ezandrobueno@yahoo.com.br>
2022-10-22 21:47:22 -0300

    Entidade User e seu Resource

---
+++ webservices/src/main/java/com/buenoezandro/resources/UserResource.java
@@ -0,0 +1,18 @@
+package com.buenoezandro.resources;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.buenoezandro.entities.User;
+
+@RestController
+@RequestMapping(value = "/users")
+public class UserResource {
+	@GetMapping
+	public ResponseEntity<User> findAll() {
+		User user = new User(1L, "Maria", "maria@gmail.com", "99999999999", "12345");
+		return ResponseEntity.ok().body(user);
+	}
+}
