dfbc6d3633c2e46a3171d4fd14fff64b2d616722
ezbueno <ezandrobueno@yahoo.com.br>
2022-10-26 14:45:49 -0300

    JPA Repository, Injeção de Dependência, Database Seeding

---
+++ webservices/src/main/java/com/buenoezandro/config/TestConfig.java
@@ -0,0 +1,28 @@
+package com.buenoezandro.config;
+
+import java.util.Arrays;
+
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
+
+import com.buenoezandro.entities.User;
+import com.buenoezandro.repositories.UserRepository;
+
+@Configuration
+@Profile(value = "test")
+public class TestConfig implements CommandLineRunner {
+	private final UserRepository userRepository;
+
+	public TestConfig(UserRepository userRepository) {
+		this.userRepository = userRepository;
+	}
+
+	@Override
+	public void run(String... args) throws Exception {
+		User u1 = new User(null, "Maria Brown", "maria@gmail.com", "988888888", "123456");
+		User u2 = new User(null, "Alex Green", "alex@gmail.com", "977777777", "654321");
+
+		this.userRepository.saveAll(Arrays.asList(u1, u2));
+	}
+}
