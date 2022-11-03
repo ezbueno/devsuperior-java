835fcd990e2d516f387a866a813ae3ec679af7f5
ezbueno <ezandrobueno@yahoo.com.br>
2022-11-02 22:10:39 -0300

    Camada de serviço, registro de componentes

--- webservices/src/main/java/com/buenoezandro/resources/UserResource.java
+++ webservices/src/main/java/com/buenoezandro/resources/UserResource.java
@@ -1,18 +1,34 @@
 package com.buenoezandro.resources;
 
+import java.util.List;
+
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.buenoezandro.entities.User;
+import com.buenoezandro.services.UserService;
 
 @RestController
 @RequestMapping(value = "/users")
 public class UserResource {
+	private final UserService userService;
+
+	public UserResource(UserService userService) {
+		this.userService = userService;
+	}
+
 	@GetMapping
-	public ResponseEntity<User> findAll() {
-		User user = new User(1L, "Maria", "maria@gmail.com", "99999999999", "12345");
+	public ResponseEntity<List<User>> findAll() {
+		List<User> list = this.userService.findAll();
+		return ResponseEntity.ok().body(list);
+	}
+
+	@GetMapping(value = "/{id}")
+	public ResponseEntity<User> findById(@PathVariable Long id) {
+		User user = this.userService.findById(id);
 		return ResponseEntity.ok().body(user);
 	}
 }
---
+++ webservices/src/main/java/com/buenoezandro/services/UserService.java
@@ -0,0 +1,25 @@
+package com.buenoezandro.services;
+
+import java.util.List;
+
+import org.springframework.stereotype.Service;
+
+import com.buenoezandro.entities.User;
+import com.buenoezandro.repositories.UserRepository;
+
+@Service
+public class UserService {
+	private final UserRepository userRepository;
+
+	public UserService(UserRepository userRepository) {
+		this.userRepository = userRepository;
+	}
+
+	public List<User> findAll() {
+		return this.userRepository.findAll();
+	}
+	
+	public User findById(Long id) {
+		return this.userRepository.findById(id).orElse(null);
+	}
+}
