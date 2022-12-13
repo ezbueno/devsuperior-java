b2ab84cc03879616812e7c346535976d1e4df3f9
ezbueno <ezandrobueno@yahoo.com.br>
2022-12-13 13:23:04 -0300

    Entidade Payment, associação um para um

--- webservices/src/main/java/com/buenoezandro/config/TestConfig.java
+++ webservices/src/main/java/com/buenoezandro/config/TestConfig.java
@@ -10,6 +10,7 @@ import org.springframework.context.annotation.Profile;
 import com.buenoezandro.entities.Category;
 import com.buenoezandro.entities.Order;
 import com.buenoezandro.entities.OrderItem;
+import com.buenoezandro.entities.Payment;
 import com.buenoezandro.entities.Product;
 import com.buenoezandro.entities.User;
 import com.buenoezandro.entities.enums.OrderStatus;
@@ -78,5 +79,10 @@ public class TestConfig implements CommandLineRunner {
 		OrderItem oi4 = new OrderItem(o3, p5, 2, p5.getPrice());
 
 		this.orderItemRepository.saveAll(Arrays.asList(oi1, oi2, oi3, oi4));
+
+		Payment pay1 = new Payment(null, Instant.parse("2022-06-20T21:53:07Z"), o1);
+		o1.setPayment(pay1);
+
+		this.orderRepository.save(o1);
 	}
 }
